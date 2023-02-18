886631127f0c0b81207d3d93c20827293a31a5e7
Mmenyie <kekanem72@gmail.com>
2023-02-18 06:23:35 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints alphabets
+ *
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+
+}
