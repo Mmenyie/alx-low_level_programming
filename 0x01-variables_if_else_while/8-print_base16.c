24c857c29e2a476673e8d37c932d5f8acee066a9
Mmenyie <kekanem72@gmail.com>
2023-02-18 16:03:56 -0800

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - a programm that prints the numbers in base 16
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+		int num;
+		char alp;
+
+		for (num = 0; num <= 9, num++;)
+			putchar(num + '0');
+
+		for (alp = 'a'; alp <= 'f'; alp++;)
+			putchar(alp);
+
+		putchar('\n');
+
+}
