af68ea7c1a6331f3313fc1a70bfd38ab5e1a6885
Mmenyie <kekanem72@gmail.com>
2023-02-19 06:47:33 -0800

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,14 +11,14 @@ int main(void)
 		int num;
 		char alp;
 
-		for (num = 0; num <= 9, num++;)
+		for (num = 0; num < 10; num++)
 			putchar((num % 10) + '0');
 
-		for (alp = 'a'; alp <= 'f'; alp++;)
+		for (alp = 'a'; alp <= 'f'; alp++)
 			putchar(alp);
 
 		putchar('\n');
-		
+
 		return (0);
 
 }
