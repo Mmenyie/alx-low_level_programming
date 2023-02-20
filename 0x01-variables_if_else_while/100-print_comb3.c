dd0590562a0e3eb407029b47eb850f8348fe4fcd
Mmenyie <kekanem72@gmail.com>
2023-02-20 03:24:43 -0800

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,23 +8,34 @@
  */
 int main(void)
 {
-	int x, y;
+	int x, y = '0', z;
 
-	for (x = 0; x < 100; x++)
-
-	for ((y = x + 1); y <= 99; y++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		putchar((x % 10) + '0');
-		putchar((y % 10) + '0');
-
-		if (x == 98 && y == 99)
-			break;
-
-		putchar(' ');
-		putchar(',');
+		for (z = y; z <= '9'; z++)
+		{
+			if (x != z)
+			{
+				putchar(x);
+				putchar(z);
+			}
+			if (x == z)
+			{
+				continue;
+			}
+			if (x == '8' && z == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		y++;
 	}
-
 	putchar('\n');
-
 	return (0);
 }
+
