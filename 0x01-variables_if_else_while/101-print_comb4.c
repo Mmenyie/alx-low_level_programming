7e084bfb060472aa8d640049762f6aefb702d0cf
Mmenyie <kekanem72@gmail.com>
2023-02-20 02:51:30 -0800

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,30 +9,32 @@
 int main(void)
 {
 	int x, y, z;
+
+	for (x = '0'; x <= '9'; x++)
 	{
-
-		for (x = '0'; x <= '9'; x++)
 		for (y = '0'; y <= '9'; y++)
-		for (z = '0'; z <= '9'; z++)
 		{
-			if (x < y && y < z)
-				putchar(x);
-				putchar(y);
-				putchar(z);
-
-
-			if (x == '7' && y == '8' && z == '9')
-				break;
-
-			else
-				putchar(' ');
-				putchar(',');
+			for (z = '0'; z <= '9'; z++)
+			{
+				if (x < y && y < z)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x == '7' && y == '8' && z == '9')
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
-
-		putchar('\n');
-
-		return (0);
 	}
-
+	putchar ('\n');
+				return (0);
 }
 
