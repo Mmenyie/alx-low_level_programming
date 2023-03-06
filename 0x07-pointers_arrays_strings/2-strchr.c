93c408249d508ae344f84248369412b4eb794266
Mmenyie <kekanem72@gmail.com>
2023-03-06 13:36:24 -0800

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,15 +12,11 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s)
-	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
-	}
-	if (c == '\0')
-		return (s);
-
-	return (NULL);
+	int i = 0;
+        for (; s[i] >= '\0'; i++)
+        {
+                if (s[i] == c)
+                        return (&s[i]);
+        }
+        return (0);
 }
