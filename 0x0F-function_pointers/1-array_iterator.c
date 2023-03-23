bfa46ab8d676e0ab9e038c33f08216b55cdca7fc
Mmenyie <kekanem72@gmail.com>
2023-03-22 17:17:15 -0700

    int_index

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
 
  /**
- * array_iterator - prints each array elem on a newline
+ * array_iterator - function given as a parameter on each of an array
+ *
  * @array: array to execute function
- * @size: how many elem to print
- * @action: pointer to print in regular or hex
+ * @size: size of the array
+ * @action: pointer to the function
+ *
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
