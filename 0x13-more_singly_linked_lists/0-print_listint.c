5f6d94a9f438d9818815354b88dc114d2038e188
Mmenyie <kekanem72@gmail.com>
2023-04-04 15:30:29 -0700

    0-print

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+More singly linked lists. How to use linked lists
