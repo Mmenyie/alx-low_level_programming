d0a12d2ad11bc2dbd7f21af6aafb91a40ed6604a
Mmenyie <kekanem72@gmail.com>
2023-04-05 06:26:34 -0700

    Tasks

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,7 +6,7 @@
   * add_nodeint_end - add node at end of a listint_t list.
   *
   * @head: head of double pointer
-  * @n: int add the list
+  * @n: int added to the list
   * Return: NULL if it failed
   */
 listint_t *add_nodeint_end(listint_t **head, const int n)
